f24a29ecc6b0a67128c1c5a35da7d5cb5ffa0839
HaLinh <haduclinhbkit@gmail.com>
2021-06-10 18:20:50 +0700

    udpate environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='tg_bot',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='auth',
+author_email='kazenki@gmail.com',
+license='MIT',
+packages=['tg_bot'],
+zip_safe=False)
--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -5,8 +5,10 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from tg_bot import DB_URI
 
 
+# engine = create_engine("mysql://root:root@127.0.0.1/tgbot",encoding='latin1', echo=True)
+
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
